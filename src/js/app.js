41796de4eee5776e90cf39fe42884c768dabf1f9
S <470319@cognizant.com>
2024-01-13 12:08:24 +0530

    netlify form

--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,37 @@
 
-$(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
-        loop: true,
-        margin: 0,
-        navText: [],
-        center: true,
-        autoplay: true,
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            1000: {
-                items: 3
-            }
+// $(document).ready(function () {
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    navText: [],
+    center: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        1000: {
+            items: 3
         }
-    });
-
-    $("button.nav-link").click(function () {
-        const id = $($(this)[0]).attr("data-link")
-        // $("navbarSupportedContent").collapse('hide');
-        $('html, body').animate({
-            scrollTop: ($("#" + id).offset().top) - 90
-        }, 750);
-    });
-
-    $('#scrollTop').fadeOut();
-    $("#scrollTop").click(function () {
-        $("html, body").animate({ scrollTop: 0 }, 750);
-    });
+    }
 });
+
+$("button.nav-link").click(function () {
+    const id = $($(this)[0]).attr("data-link")
+    $('html, body').animate({
+        scrollTop: ($("#" + id).offset().top) - 90
+    }, 750);
+});
+
+$('#scrollTop').fadeOut();
+$("#scrollTop").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 750);
+});
+$('.navbar-toggler').on('click', function () {
+    $('.navbar .collapse').collapse('toggle');
+})
+// });
 $(window).scroll(function () {
     if ($(this).scrollTop()) {
         $('#scrollTop').fadeIn();
